d4c929960604a5c9a851c81ff433388c9b5ec14b
Muhammad Hamza <22521243+mhamzaib@users.noreply.github.com>
2022-09-21 01:07:07 +0500

    Added skeleton for functions

---
+++ randhasher/functions.py
@@ -0,0 +1,42 @@
+import hashlib as hl
+
+class HashTypes:
+    sha = ['sha1', 'sha224', 'sha256', 'sha384', 'sha512']
+    sha3 = ['sha3_224', 'sha3_256', 'sha3_384', 'sha3_512()']
+    blakes = ['blake2b', 'blake2s']
+    others = ['shake_128', 'shake_256', 'md5']
+
+def generateSha(string, noHex=False, types=[]):
+    compiled_hashes=[]
+    
+    return
+
+def generateSha3(string, noHex=False, types=[]):
+    compiled_hashes=[]
+    
+    return
+
+def generateAllSha(string, noHex=False, types=[]):
+    compiled_hashes=[]
+    
+    return
+
+def generateBlakes(string, noHex=False, types=[]):
+    compiled_hashes=[]
+    
+    return
+
+def generateOtherHashes(string, noHex=False, types=[]):
+    compiled_hashes=[]
+    
+    return
+
+def generateCombination(string, types, noHex=False):
+    compiled_hashes=[]
+    
+    return
+
+def generateAll(string, noHex=False):
+    compiled_hashes=[]
+    
+    return
